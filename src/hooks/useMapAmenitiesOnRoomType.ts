11c550ab87e10ed84c28d8136d93c7cd89f58801
joaquin.pettinari <joaquin.pettinari@hotmail.com>
2026-03-11 21:50:54 +0100

    Adding amenities and fit image

---
+++ src/hooks/useMapAmenitiesOnRoomType.ts
@@ -0,0 +1,22 @@
+import { useCallback } from "react";
+import type { Amenity } from "../types/rooms";
+import { useRoomsAmenities } from "../api/rooms";
+
+const findAmenityForRoomType = (roomTypeId: number, amenities: Amenity[]) => {
+  return amenities.find((amenity) => amenity.id === roomTypeId);
+};
+
+export const useMapAmenitiesOnRoomType = () => {
+  const { data: amenities = [] } = useRoomsAmenities();
+
+  const mapAmenitiesOnRoomType = useCallback(
+    (amenityId: number) => {
+      return findAmenityForRoomType(amenityId, amenities);
+    },
+    [amenities],
+  );
+
+  return {
+    mapAmenitiesOnRoomType,
+  };
+};
--- src/pages/Home/RoomDetails/RoomDetail.tsx
+++ src/pages/Home/RoomDetails/RoomDetail.tsx
@@ -2,9 +2,11 @@ import { CircularProgress, Grid } from "@mui/material";
 import { useRooms } from "../../../api/rooms";
 import { RoomCard } from "../../../components/RoomCard/RoomCard";
 import { noop } from "lodash";
+import { useMapAmenitiesOnRoomType } from "../../../hooks/useMapAmenitiesOnRoomType";
 
 function RoomDetails() {
   const { data: rooms, isLoading } = useRooms();
+  const { mapAmenitiesOnRoomType } = useMapAmenitiesOnRoomType();
 
   if (isLoading) return <CircularProgress />;
   if (!rooms) return <span>No rooms</span>;
@@ -12,7 +14,12 @@ function RoomDetails() {
   return (
     <Grid container flexDirection={"column"} gap={3}>
       {rooms.map((room) => (
-        <RoomCard key={room.id} room={room} onEdit={noop} />
+        <RoomCard
+          key={room.id}
+          room={room}
+          onEdit={noop}
+          mapAmenitiesOnRoomType={mapAmenitiesOnRoomType}
+        />
       ))}
     </Grid>
   );
